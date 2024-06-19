34e56753f4e506216d19f2871889138769e9de7d
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-06-19 10:17:54 +0200

    refactor: Make tile_still_valid a little less magic by relying on a more generic "sources" field instead of a magic "tile_position" field

--- std-routines-v1/predicates.lua
+++ std-routines-v1/predicates.lua
@@ -1,9 +1,23 @@
 local Predicates = {}
 
-function Predicates.tile_still_valid(tile_id)
+function Predicates.sources_still_valid()
     return function(entity_id, data)
-        return get_tile(data.tile_position) == tile_id
+        for _, source in ipairs(data.sources) do
+            if source.type === "tile" then
+                if get_tile(source.position) == source.tile_id then
+                    return true
+                end
+            end
+        end
     end
+end
+
+function Predicates.create_tile_source(tile_id, tile_position)
+    return {
+        type = "tile",
+        tile_id = tile_id,
+        position = tile_position
+    }
 end
 
 return Predicates
